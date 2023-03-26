2abd7cbe3b4c42181d11ede249c46c537eca03a0
Rohit <rohitkumarr050@gmail.com>
2023-03-26 09:31:46 +0530

    implement  eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+  ],
+  plugins: ['prettier'],
+  parserOptions: {
+      ecmaVersion: 'latest',
+  },
+  rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+      'no-restricted-syntax': 'off',
+      'no-prototype-builtins': 'off',
+  },
+};
